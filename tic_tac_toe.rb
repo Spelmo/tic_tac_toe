dac95509b9f90389d64feb0dee55c3b8d678c55a
MC Le Morvan <mclemorvan@gmail.com>
2018-10-16 09:36:25 +0200

    add file

---
+++ tic_tac_toe.rb
@@ -0,0 +1,7 @@
+le programme est à deux joueurs only
+le programme doit commencer en demandant le prénom des joueurs
+le programme doit afficher le plateau chaque tour (dans le terminal),
+puis demander au joueur auquel c'est le tour où ce dernier compte jouer
+si un joueur gagne, le programme doit annoncer qui a gagné
+s'il y a match nul, le programme doit annoncer qu'il y a match nul
+À la fin d'une partie, le programme doit proposer une autre partie
